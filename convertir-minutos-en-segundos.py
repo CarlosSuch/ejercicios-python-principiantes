9c070121e28f862be8a8030b4b537f95932e17fc
CarlosSuch <alicarlossuch@gmail.com>
2023-02-19 13:54:02 +0100

    Añadimos ejercicio de convertir minutos a segundos

---
+++ convertir-minutos-en-segundos.py
@@ -0,0 +1,21 @@
+'''
+__________CONVERTIR MINUTOS EN SEGUNDOS__________
+
+Escribe una función que pase un número entero de minutos y los convierta en segudos.
+
+'''
+# 1 solución 
+def minutos_segundos(num):
+    return num * 60
+
+print(minutos_segundos(25))
+
+
+# 2 solución
+def de_minutos_a_segundos(numero):
+    resultado = numero * 60
+    return resultado
+
+print(de_minutos_a_segundos(25))
+
+
