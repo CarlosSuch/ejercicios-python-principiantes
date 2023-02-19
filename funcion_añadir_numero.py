cb4fe3cffcb5a39a9281468a8bd57e4fb8ba8da1
CarlosSuch <alicarlossuch@gmail.com>
2023-02-19 14:11:24 +0100

    Ejercicio incrementar + 1 a un número

--- funcion_añadir_numero.py
+++ funcion_añadir_numero.py
@@ -7,14 +7,13 @@ incrementar(2)   -> 3
 incrementar(8)   -> 9
 incrementar(-2)  -> -1
 
-
 '''
 
 # Mi solución
 
 def incrementar(numero):
     return numero + 1
-
+# Mostramos por pantalla un resultado, en este caso elegimos el número 89 para que le sume 1. 
 print(incrementar(89))
 
 # 2 Solución
@@ -22,5 +21,5 @@ print(incrementar(89))
 def incremento(num):
     num = num + 1
     return num 
-
+# Mostramos por pantalla otro resultado, en este caso con el número 8. 
 print(incremento(8))
