55c4e488dcc691fbbbf587e7783ffea5e9ff44cd
CarlosSuch <alicarlossuch@gmail.com>
2023-02-19 12:55:25 +0100

    primer commit

---
+++ funcion-sumar-dos-numeros.py
@@ -0,0 +1,25 @@
+'''
+   ____________  DEVUELVE LA SUMA DE DOS NÚMEROS ______________
+
+   ¿Qué tienes que hacer?
+
+   1. Crear una función
+   2. Tomar dos números como argumentos
+   3. Devolver la suma
+   4. Usar return en el resultado
+
+ejemplos:
+suma(4, 9)      ->  13
+suma(-5, -10)   -> -15
+
+'''
+
+# Mi solución
+def suma(a, b):
+    return a + b
+
+
+# Otra solución 
+def suma_dos(numero1, numero2):
+    sumita = numero1 + numero2
+    return sumita
