bd7f57207547d2d73b9fffdb916340242259800a
CarlosSuch <alicarlossuch@gmail.com>
2023-02-19 14:36:59 +0100

    Ejercicio de función que incremente el valor en + 1

--- funcion_añadir_numero.py
+++ funcion_añadir_numero.py
@@ -6,18 +6,14 @@ En este ejercicio se pide que se cree una función, que se tomará como argument
 incrementar(2)   -> 3
 incrementar(8)   -> 9
 incrementar(-2)  -> -1
-
 '''
-
 # Mi solución
-
 def incrementar(numero):
     return numero + 1
 # Mostramos por pantalla un resultado, en este caso elegimos el número 89 para que le sume 1. 
 print(incrementar(89))
 
 # 2 Solución
-
 def incremento(num):
     num = num + 1
     return num 
