d1864ad5d3d8a6e3e9fe3b2a095df5b50d18b057
CarlosSuch <alicarlossuch@gmail.com>
2023-02-19 13:19:24 +0100

    Añadido ejercicio para incrementar valor

---
+++ funcion_añadir_numero.py
@@ -0,0 +1,26 @@
+'''
+______________Devolver el siguiente número_____________
+
+En este ejercicio se pide que se cree una función, que se tomará como argumento, y tenemos que incrementar su valor en 1. 
+
+incrementar(2)   -> 3
+incrementar(8)   -> 9
+incrementar(-2)  -> -1
+
+
+'''
+
+# Mi solución
+
+def incrementar(numero):
+    return numero + 1
+
+print(incrementar(89))
+
+# 2 Solución
+
+def incremento(num):
+    num = num + 1
+    return num 
+
+print(incremento(8))
